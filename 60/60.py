1aa7d61a7b06e09e17abc6741a81fe1c26c844c6
fssqawj <fssqawj@gmail.com>
2019-01-29 15:30:35 +0800

    add comment

--- 60/60.py
+++ 60/60.py
@@ -12,22 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import codecs
-
-
-def get_ord_sum(x):
-    return sum([ord(c) for c in x])
 
 
 if __name__ == '__main__':
-    with codecs.open('p059_cipher.txt', 'r', encoding='utf-8') as fin:
-        line = [int(x) for idx, x in enumerate(fin.read().strip().split(','))]
-        num_cnt = {x: line.count(x) for x in line}
-        print(sorted(num_cnt.items(), key=lambda x: x[1]))
-        # three char generate the space which count most
-        key = 'god'
-        res = ''
-        for idx, x in enumerate(line):
-            res += chr(ord(key[idx % 3]) ^ x)
-        print(res)
-        print(get_ord_sum(res))
+    pass
