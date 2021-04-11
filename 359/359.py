11974649f3bac60defa2ff68ef1d87fcbbfe0385
fssqawj <fssqawj@gmail.com>
2021-04-11 11:36:02 +0800

    solve 359

--- 359/359.py
+++ 359/359.py
@@ -47,28 +47,52 @@ def find_fir(limit):
 
 def get_factors(x):
     res = []
-    for i in tqdm(range(2, int(math.sqrt(x + 1)) + 1)):
+    for i in tqdm(range(1, int(math.sqrt(x + 1)) + 1)):
         if x % i == 0:
             res.append(i)
     return res
 
 
+def calc(i, j):
+    if i == 1:
+        a = 2
+        fir = 1
+    elif i % 2 == 1:
+        a = i
+        idx = i // 2
+        fir = 2 * idx * idx + 2 * idx
+    else:
+        a = i + 1
+        idx = i // 2
+        p_idx = idx - 1
+        fir = (idx * idx + idx) + (p_idx * p_idx + p_idx)
+    if j % 2 == 1:
+        return fir + (j - 1) // 2 * (j + 2 * a - 2)
+    return (a + j - 2) * (a + j - 2) - fir - (j - 2) // 2 * (j + 2 * a - 3)
+
+
 def main():
-    limit = 1000
+    # limit = 1000
     n = 71328803586048
-    firs = find_fir(limit)
+    # firs = find_fir(limit)
     factors = get_factors(n)
     print(factors)
-    print(firs)
-    res = set()
-    for fir in firs:
-        cnt = 0
-        cur = fir
-        while cnt < 100:
-            print(cur, end=", ")
-            cur = find(cur)
-            cnt += 1
-        print()
+    # print(firs)
+    # res = set()
+    # for fir in firs:
+    #     cnt = 0
+    #     cur = fir
+    #     while cnt < 100:
+    #         print(cur, end=", ")
+    #         cur = find(cur)
+    #         cnt += 1
+    #     print()
+    # print(calc(99, 100))
+    res = 0
+    for factor in factors:
+        res += calc(factor, n // factor)
+        res += calc(n // factor, factor)
+    print(res)
 
 
 if __name__ == '__main__':
